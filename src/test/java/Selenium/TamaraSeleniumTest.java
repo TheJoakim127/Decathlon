59342afd57669da05827412cf9fb14f3863d009b
tamaraalsaad <tamaraalsaad@yahoo.com>
2026-03-30 11:56:05 +0100

    Ny

--- src/test/java/Selenium/TamaraSeleniumTest.java
+++ src/test/java/Selenium/TamaraSeleniumTest.java
@@ -4,18 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class JoakimSeleniumTest {
+public class TamaraSeleniumTest {
 
     @Test
     public void tricentisOpenTest() {
