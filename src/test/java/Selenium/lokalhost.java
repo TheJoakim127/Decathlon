f89df1556f1b01687e6b3aa3ce67c741b12ebf35
soire <soireelaine@gmail.com>
2026-03-30 10:22:16 +0200

    initial commit

---
+++ src/test/java/Selenium/lokalhost.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class lokalhost {
+    @Test
+    public void testoppenlokalhost(){
+        WebDriver driver= new ChromeDriver();
+        driver.get();
+    }
+
+}
