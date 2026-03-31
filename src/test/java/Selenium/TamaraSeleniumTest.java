0af55d4c1a36014ce7b30ca469914fabc36b4432
tamaraalsaad <tamaraalsaad@yahoo.com>
2026-03-31 08:22:03 +0100

    Ny

--- src/test/java/Selenium/TamaraSeleniumTest.java
+++ src/test/java/Selenium/TamaraSeleniumTest.java
@@ -1,7 +1,9 @@
 package Selenium;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +16,17 @@ public class TamaraSeleniumTest {
     public void tricentisOpenTest() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8080/");
+        driver.findElement(By.id("modeHep")).click();
     }
+    @Test
+    public void AddCompetitor () {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/");
+        WebElement Add = driver.findElement(By.id("name"));
+        Add.sendKeys("Anna");
+
+
+    }
+
+
 }
