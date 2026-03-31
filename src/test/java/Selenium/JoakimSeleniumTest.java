fe6009e674a67bc6365c761e0b5efd6fe3ae337d
joaki <joakim_holmstrom8@hotmail.com>
2026-04-01 00:20:30 +0200

    Test7

--- src/test/java/Selenium/JoakimSeleniumTest.java
+++ src/test/java/Selenium/JoakimSeleniumTest.java
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
 
 
 public class JoakimSeleniumTest {
@@ -22,5 +25,16 @@ public class JoakimSeleniumTest {
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8080/");
         driver.findElement(By.id("modeHep")).click();
+        driver.findElement(By.id("name")).sendKeys("Anna");
+        driver.findElement(By.id("add")).click();
+        WebElement dropdown = driver.findElement(By.id("event"));
+        Select select = new Select(dropdown);
+        select.selectByValue("hepHighJump");
+        driver.findElement(By.id("raw")).sendKeys("250");
+        driver.findElement(By.id("save")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        String total = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='standingsTable'] tr td:last-child"))).getText();
+        assertEquals("1948", total);
+        driver.quit();
     }
 }
