9f8959e95a27ebf102e98eb6fd5a49c318b8c31e
joaki <joakim_holmstrom8@hotmail.com>
2026-04-13 12:13:42 +0200

    Test7

--- src/test/java/Selenium/JoakimSeleniumTest.java
+++ src/test/java/Selenium/JoakimSeleniumTest.java
@@ -1,99 +1,13 @@
 package Selenium;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class JoakimSeleniumTest {
-    @Test
-    public void testoppenlokalhost() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/");
-
-        driver.findElement(By.id("modeHep")).click();
-        driver.findElement(By.id("name")).sendKeys("Cici");
-        driver.findElement(By.id("add")).click();
-
-        Select select = new Select(driver.findElement(By.id("event")));
-        select.selectByVisibleText("100m Hurdles (s)");
-
-        driver.findElement(By.id("raw")).sendKeys("9");
-        driver.findElement(By.id("save")).click();
-    }
-
-
-    //auto test all event
-    @Test
-    public void testAllHeptathlonEvents() {
-        WebDriver driver = new ChromeDriver();
-
-        try {
-            driver.get("http://localhost:8080/");
-
-            // Väljer Heptathlon
-            driver.findElement(By.id("modeHep")).click();
-
-            // Skriver in namn
-            driver.findElement(By.id("name")).sendKeys("Cici");
-
-            // Lägger till en deltagare
-            driver.findElement(By.id("add")).click();
-
-            // Testdata för olika grenar
-            Map<String, String> testData = new HashMap<>();
-            testData.put("100m Hurdles (s)", "9");
-            testData.put("High Jump (cm)", "150");
-            testData.put("Shot Put (m)", "10");
-            testData.put("200m (s)", "25");
-            testData.put("Long Jump (cm)", "500");
-            testData.put("Javelin Throw (m)", "30");
-            testData.put("800m (s)", "70");
-
-            // Testar varje gren med ett värde
-            for (String event : testData.keySet()) {
-                Select eventSelect = new Select(driver.findElement(By.id("event")));
-                eventSelect.selectByVisibleText(event);
-
-                WebElement rawInput = driver.findElement(By.id("raw"));
-                rawInput.clear();
-                rawInput.sendKeys(testData.get(event));
-
-                driver.findElement(By.id("save")).click();
-
-                System.out.println("Testade: " + event + " -> " + testData.get(event));
-            }
-
-        } finally {
-            // Stänger webbläsaren
-            // driver.quit();
-        }
-    }
-
-    /*WebDriver driver;
-    //explicit wait: Väntar tills ett element syns
-    private WebElement waitForElement(By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }*/
-}
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -105,12 +19,6 @@ import java.time.Duration;
 
 
 public class JoakimSeleniumTest {
-    private WebDriverWait wait;
-    public WebElement waitForVisible(By locator) {
-
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-
-    }
 
     @Test
     public void tricentisOpenTest() {
@@ -133,46 +41,4 @@ public class JoakimSeleniumTest {
         assertEquals("Anna", name);
         driver.quit();
     }
-
-    @Test
-    public void AddCompetitor() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/");
-        driver.findElement(By.id("modeHep")).click();
-        WebElement Add = driver.findElement(By.id("name"));
-        Add.sendKeys("Anna");
-
-    }
-
-    @Test
-    public void EnterResultHighJump() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/");
-        driver.findElement(By.id("modeHep")).click();
-        WebElement Add = driver.findElement(By.id("name"));
-        Add.sendKeys("Anna");
-        driver.findElement(By.id("add")).click();
-        driver.findElement(new By.ByXPath("/html/body/div[3]/div[1]/select/option[2]")).click();
-        driver.findElement(By.id("raw")).sendKeys("100");
-        driver.findElement(By.id("save")).click();
-
-
-
-    }
-    @Test
-    public void EnterResultShotPut() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/");
-        driver.findElement(By.id("modeHep")).click();
-        WebElement Add = driver.findElement(By.id("name"));
-        Add.sendKeys("Josef");
-        driver.findElement(By.id("add")).click();
-        driver.findElement(new By.ByCssSelector("#event > option:nth-child(3)")).click();
-        driver.findElement(By.id("raw")).sendKeys("100");
-        driver.findElement(By.id("save")).click();
-
-
-
-    }
 }
-
